7a1ae15cb389681bb52d52a84ac01c0d7cb44b77
Gilberter <juanchodve14@gmail.com>
2024-08-31 13:33:50 -0500

    Casa

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const path = express('path');
-
 const app = express();
-const ejs = require('ejs');
+const path = require('path');
+const ejs = require('ejs')
+const bodyParser = require('body-parser');
+
+app.set('views',path.resolve(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+var entries = [];
+app.locals.entries = entries;
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index');
 })
-app.listen(8080,    () => console.log("Running"))
+app.get('/add-Post',(req,res) =>{
+    res.render('publisher');
+})
+app.post('/add-Post', (req,res) => {
+    if(!req.body.title || !req.body.content){
+        res.status(400).send("Entries must have a tittle and a body");
+        return;
+    }
+    entries.push({
+        title: req.body.title,
+        content: req.body.content
+    });
+    res.redirect('/');
+})
+app.listen(8080,() => console.log("Running"))
